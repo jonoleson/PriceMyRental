5ceb6560b6f9b2fc3728aed9915642d37b87dc6a
gSchool Student <gSchool@Datas-Mac-mini-14.local>
2015-06-13 21:24:06 -0700

    Update get_data code

--- get_data.py
+++ get_data.py
@@ -16,9 +16,15 @@ def get_data():
   #Get dataframe of cities
   cities = pd.read_csv('ongoing_cities.csv', headers = True)
 
+  ''' 
+  Target headers will look like:
   headers = ["id", "header", "body", "bedrooms", "bathrooms", "sqft", 
              "timestamp", "price", "external_url", "lat", "lon", 
              "accuracy", "address", "parking", "washer_dryer", "pet"]   
+  '''
+  # Headers for the dataframes loaded by pd.read_json()
+  headers = ['annotations', 'body', 'category', 'expires', 'external_url', 'flagged_status', 
+             'heading', 'id', 'immortal', 'location', 'price', 'source', 'timestamp']
 
   for row in cities:
     city = row.city_code
@@ -27,37 +33,57 @@ def get_data():
 
     #Append data to city_df
     for date in daterange(start_date, end_date):
-      i=0
-      while i:
+      done_parsing = False
+
+      while done_parsing = False:
         url = 'https://s3-us-west-2.amazonaws.com/hoodsjson/%s/%s/%s/%s.html' %(state, city, date, i)
-        r = requests.get(url)
-        data_hash = r.json() 
-        if len(data_hash) > 0:
-          results = parse_info(data_hash)
+        
+        #Read json into pandas dataframe
+        raw_df = pd.read_json(url)
+        #Filter df for only craigslist data
+        condition = raw_df['source'] == 'CRAIG'
+        raw_df = raw_df[condition]
+
+        #Test if loaded df has any data
+        if len(raw_df) > 0:
+          results = parse_info(raw_df, headers)
           results_df = pd.DataFrame(results)
           city_df = pd.concat(city_df, results_df)
           i=+1   
         else:
-          break
+          done_parsing = True
 
     #Write city_df to a csv 
     city_df.to_csv('csvs/%s_%s.csv', 'wb+') %(city, state)
 
-def parse_info(data_hash, headers):
-  results_df = pd.DataFrame(columns = headers)
-  for apt in data_hash:
-    header = apt_info['heading']
-    body = apt_info['body']
-    timestamp = Time.at(apt_info['timestamp'].to_i).utc.to_datetime.to_s[0..9]  
-    price = apt_info['price']
-    external_url = apt_info['external_url']
-    lat = apt_info['location']['lat']
-    lon = apt_info['location']['long']
-    accuracy = apt_info['location']['accuracy']
-    address = apt_info['location']['formatted_address']
-    rooms = apt_info['annotations']['bedrooms']
-    parking = apt_info['annotations']['carport']
-    washer_dryer = apt_info['annotations']['w_d_in_unit']
+def parse_info(df, headers):
+  results_df = pd.DataFrame(columns = headers, index = xrange(len(df)))
+  for i, apt in enumerate(df):
+    results_df['header'][i] = apt['heading']
+    results_df['body'][i] = apt['body']
+    results_df['price'][i] = apt['price']
+    results_df['lat'][i] = apt['location']['lat']
+    results_df['lon'][i] = apt['location']['long']
+    results_df['accuracy'][i] = apt['location']['accuracy']
+    results_df['address'][i] = apt['location']['formatted_address']
+    results_df['beds'][i] = apt['annotations']['bedrooms']
+    results_df['baths'][i] = apt['annotations']['bathrooms']
     
+    if 'street_parking' in apt['annotations'][0]:
+      results_df['parking'][i] = 1
+    elif 'carport' in apt['annotations'][0]:
+      results_df['parking'][i] = 2
+    elif 'off_street_parking' in apt['annotations'][0]:
+      results_df['parking'][i] = 3
+    elif 'attached_garage' in apt['annotations'][0]:
+      results_df['parking'][i] = 4
+    else:
+      results_df['parking'][i] = 0
+
+    results_df['parking'][i] = apt['annotations']['carport']
+    results_df['washer_dryer'][i] = apt['annotations']['w_d_in_unit']
+
+  results_df['date'] = pd.to_datetime(df['timestamp'], unit='s')
+  return results_df
 
 
