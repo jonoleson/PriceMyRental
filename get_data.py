ae6bced0ca1265f9058ede878d6e07038781ae73
gSchool Student <gSchool@Datas-Mac-mini-2.local>
2015-06-16 15:26:39 -0700

    Add updated notes and eda notebook

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import timedelta, date
+import urllib2
 
 
 #Function to iterate through dates
@@ -8,17 +9,25 @@ def daterange(start_date, end_date):
         single_date = start_date + timedelta(n)
         yield single_date.strftime('%Y-%m-%d')
 
-def get_data(start_date, end_date):
+def get_data(start_date, end_date, one_city=False):
   #Get dataframe of cities
   cities = pd.read_csv('data/ongoing_cities.csv', header = False)
 
   # Headers for our target dataframe
-  cols = ['heading', 'body', 'price', 'lat', 'long', 'region', 'neighborhood', 
+  cols = ['id', 'heading', 'body', 'price', 'lat', 'long', 'region', 'neighborhood', 
              'beds', 'baths', 'parking', 'washer_dryer']
 
+  #If True, parse only the data for San Francisco
+  if one_city==True:
+    cities = cities[(cities['city'] == 'San Francisco')]
+
   for i in xrange(len(cities)):
-    city = cities.city_code[i]
-    state = cities.state_code[i]
+    if one_city==False:
+      city = cities.city_code[i]
+      state = cities.state_code[i]
+    else:
+      city = cities.city_code.values[0]
+      state = cities.state_code.values[0]
     city_df = pd.DataFrame(columns=cols)
 
     #Append data to city_df
@@ -30,7 +39,13 @@ def get_data(start_date, end_date):
         url = 'https://s3-us-west-2.amazonaws.com/hoodsjson/%s/%s/%s/%s.html' %(state, city, date, k)
         print url
         #Read json into pandas dataframe
-        raw_df = pd.read_json(url)
+        #Some days may have no data and may throw an error when loaded, so using try/except to control for this
+        try: 
+          raw_df = pd.read_json(url)
+        except urllib2.HTTPError:
+          print 'No data this day'
+          break
+
 
         #Test if loaded df has any data (an empty JSON file always concludes the )
         if len(raw_df) > 0:
@@ -52,6 +67,7 @@ def get_data(start_date, end_date):
 
 def parse_info(df, cols):
   results_df = pd.DataFrame(columns = cols, index = xrange(len(df)))
+  results_df['id'] = df['id']
   results_df['heading'] = df['heading']
   results_df['body'] = df['body']
   results_df['price'] = df['price']
@@ -74,7 +90,11 @@ def parse_info(df, cols):
       results_df.ix[i, 'neighborhood'] = None
 
     if 'bedrooms' in df.iloc[i]['annotations']:
-      results_df.ix[i, 'beds'] = int(df.iloc[i]['annotations']['bedrooms'][0])
+      #A few listings say 'Studio' rather than '0br' in the bedrooms field
+      if df.iloc[i]['annotations']['bedrooms'][0] not in '0123456789':
+         results_df.ix[i, 'beds'] = 0
+      else:
+        results_df.ix[i, 'beds'] = int(df.iloc[i]['annotations']['bedrooms'][0])
     else:
       results_df.ix[i, 'beds'] = None
 
@@ -97,15 +117,14 @@ def parse_info(df, cols):
     if 'w_d_in_unit' in df.iloc[i]['annotations']:
       results_df.ix[i, 'washer_dryer'] = 1
     else:
-      results_df.ix[i, 'washer_dryer'] = 1
+      results_df.ix[i, 'washer_dryer'] = 0
 
   return results_df
 
 def main():
-  start_date = date(2015, 5, 29)
-  end_date = date(2015, 5, 30)
-
-  get_data(start_date, end_date)
+  start_date = date(2014, 9, 29)
+  end_date = date(2015, 6, 17)
+  get_data(start_date, end_date, one_city=True)
 
 if __name__=='__main__':
   main()
